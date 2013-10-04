31660188aa26290011cf6e59e4ebe4535a9b7820
tyad <tyad.goo@gmail.com>
2013-10-04 19:46:41 +0900

    reset test

--- hidamari_swing.js
+++ hidamari_swing.js
@@ -5,7 +5,7 @@ window.onload = function mogura() {
 	game.keybind(32, "space");  // spaceキー
 	game.preload(
 		//Background
-		'img/background_title.jpg',	'img/background_batting.jpg', 'img/background_batting_scale2.jpg',
+		'img/background_title.jpg',	'img/background_batting.jpg',
 		//Charactor
 		'img/logo.png',
 		'img/direction.gif',
@@ -32,8 +32,8 @@ window.onload = function mogura() {
 	var SCREEN_SIZE_X = 480;
 	var SCREEN_SIZE_Y = 480;
 	//グラウンド全体サイズ
-	var GROUND_SIZE_X = 4800;
-	var GROUND_SIZE_Y = 3200;
+	var GROUND_SIZE_X = 2400;
+	var GROUND_SIZE_Y = 1600;
 	//スタートボタン位置
 	var STARTBUTTON_X = 90;
 	var STARTBUTTON_Y = 300;
@@ -463,9 +463,11 @@ window.onload = function mogura() {
 
 	//*背景*
 		BackgroundBatting = new Sprite(GROUND_SIZE_X, GROUND_SIZE_Y);
-		BackgroundBatting.image = game.assets['img/background_batting_scale2.jpg'];
+		BackgroundBatting.image = game.assets['img/background_batting.jpg'];
 		BackgroundBatting.x = 0;
-		BackgroundBatting.y = 0;
+		BackgroundBatting.y = -800;
+		BackgroundBatting.scaleX = 2;
+		BackgroundBatting.scaleY = 2;
 
 	//*バーチャルキーパッド*
 		var KeyPad = new Pad();
