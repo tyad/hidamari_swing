54e00c02d7e92082b021220d34ba5d5f3d3d74d2
tyad <tyad.goo@gmail.com>
2013-10-13 10:55:20 +0900

    add commentout

--- cookie.js
+++ cookie.js
@@ -1,4 +1,4 @@
-//
+//通常コース生成
 function createCommonCourse(){
 	var CommonCourse = ["EASY"];
 	if(getCookie("UNLOCK-NORMAL") == 1){
@@ -15,7 +15,7 @@ function createCommonCourse(){
 	}
 	return CommonCourse;
 }
-//
+//エクストラコース生成
 function createExtraCourse(){
 	var ExtraCourse = ["KNOCK"];
 	if(getCookie("UNLOCK-YUNO")){
@@ -30,7 +30,7 @@ function createExtraCourse(){
 	return ExtraCourse;
 }
 
-//
+//数字か判定関数（undefinedとNaNを避けるため）
 function isNumber(x){ 
     if( typeof(x) != 'number' && typeof(x) != 'string' )
         return false;
@@ -38,7 +38,7 @@ function isNumber(x){
         return (x == parseFloat(x) && isFinite(x));
 }
 
-//
+//指定のクッキー取得
 function getCookie(kword){
 	kword = kword + "=";
 	kdata = "";
@@ -55,54 +55,55 @@ function getCookie(kword){
 	return parseInt(kdata);
 }
 
-//
+//指定のクッキーを設定
 function setCookie(name,value){
 	var expire = new Date();
 	expire.setTime( expire.getTime() + 1000 * 3600 * 24 * 365 * 100 );
 	document.cookie = name+'=' + encodeURIComponent( value ) + '; expires=' + expire.toUTCString();
 }
 
+//アンロック判定関数
 function unlock(){
 	console.log("unlock check");
 	//NORMAL解放
 	if(getCookie("CLEARLANK-EASY") > 0 && getCookie("UNLOCK-NORMAL") != 1){
 		setCookie("UNLOCK-NORMAL",1);
-		console.log('UNLOCK-NORMAL');
+		console.log('UNLOCK-NORMAL');//ポップアップ表示と置き換えてね
 	}
 	//HARD解放
 	if(getCookie("CLEARLANK-NORMAL") > 0 && getCookie("UNLOCK-HARD") != 1){
 		setCookie("UNLOCK-HARD",1);
-		console.log('UNLOCK-HARD');
+		console.log('UNLOCK-HARD');//ポップアップ表示と置き換えてね
 	}
 	//STRAIGHT & KIRE解放
 	if(getCookie("CLEARLANK-HARD") > 0 && getCookie("UNLOCK-STRAIGHT") != 1 && getCookie("UNLOCK-KIRE") != 1){
 		setCookie("UNLOCK-STRAIGHT",1);
 		setCookie("UNLOCK-KIRE",1);
-		console.log('UNLOCK-STRAIGHT,KIRE');
+		console.log('UNLOCK-STRAIGHT,KIRE');//ポップアップ表示と置き換えてね
 	}
 	//YUNO解放
 	if(getCookie("CLEARLANK-STRAIGHT") > 0 && getCookie("CLEARLANK-KIRE") > 0 && getCookie("UNLOCK-YUNO") != 1){
 		setCookie("UNLOCK-YUNO",1);
-		console.log('UNLOCK-YUNO');
+		console.log('UNLOCK-YUNO');//ポップアップ表示と置き換えてね
 	}
-	/*あとで
+	/*命名規則に従って変えてください
 	//KNOCK_EX解放
-	if(getCookie("CLEARLANK-STRAIGHT") > 0 && getCookie("CLEARLANK-KIRE") > 0 && getCookie("UNLOCK-YUNO") != 1){
-		setCookie("UNLOCK-YUNO",1);
-		console.log('UNLOCK-YUNO');
-	}
-	//EASY_EX解放
-	if(getCookie("CLEARLANK-STRAIGHT") > 0 && getCookie("CLEARLANK-KIRE") > 0 && getCookie("UNLOCK-YUNO") != 1){
-		setCookie("UNLOCK-YUNO",1);
-		console.log('UNLOCK-YUNO');
+	if(getCookie("なんちゃらJUSTMEET") > 0 && getCookie("UNLOCK-KNOCK_EX") != 1){
+		setCookie("UNLOCK-KNOCK_EX",1);
+		console.log('UNLOCK-KNOCK_EX');//ポップアップ表示と置き換えてね
 	}
 	*/
+	//EASY_EX解放
+	if(getCookie("CLEARLANK-EASY") == 3 && && getCookie("UNLOCK-EASY_EX") != 1){
+		setCookie("UNLOCK-EASY_EX",1);
+		console.log('UNLOCK-EASY_EX');//ポップアップ表示と置き換えてね
+	}
 }
 
-//
+//クッキー更新
 function cookieUpdate(course_e, Point, ball_number){
-	console.log(course_e);
-	console.log(Point.num);
+	console.log(course_e);//for debug
+	console.log(Point.num);//for debug
 	//◆ほのぼの
 	if(course_e === "EASY"){
 		var clearlank = getCookie("CLEARLANK-EASY");
@@ -114,10 +115,12 @@ function cookieUpdate(course_e, Point, ball_number){
 		}else if(Point.num >= 250 && clearlank < 1){//☆ 250
 			setCookie("CLEARLANK-EASY",1);
 		}
+		/*お好きにどうぞ
 		//★全真芯
 		if(Point.super_hit == ball_number){
 		
 		}
+		*/
 	}
 	//◆わくわく
 	else if(course_e === "NORMAL"){
@@ -162,10 +165,12 @@ function cookieUpdate(course_e, Point, ball_number){
 		}else if(Point.num >= 500 && clearlank < 1){//☆ 500
 			setCookie("CLEARLANK-KIRE",1);
 		}
+		/*お好きにどうぞ
 		//★空振りしない
 		if(Point.miss == 0){
 		
 		}
+		*/
 	}
 	//◆ゆのさま
 	else if(course_e === "YUNO"){
@@ -177,8 +182,10 @@ function cookieUpdate(course_e, Point, ball_number){
 		}else if(Point.num >= 400 && clearlank < 1){//☆ 400
 			setCookie("CLEARLANK-YUNO",1);
 		}
+		/*お好きにどうぞ
 		//★真芯で返す
 		//★空振りしない
+		*/
 	}
 	//◆100本ノック
 	else if(course_e === "KNOCK"){
@@ -190,9 +197,15 @@ function cookieUpdate(course_e, Point, ball_number){
 		}else if(Point.num >= 3000 && clearlank < 1){//☆ 3000
 			setCookie("CLEARLANK-KNOCK",1);
 		}
-		//★真芯20
-		//★真芯10
-		//★真芯5
+		/*お好きにどうぞ
+		if(Point.num >= 8000 && clearlank < 3){//★真芯20
+			setCookie("なんちゃらJUSTMEET",3);
+		}else if(Point.num >= 5000 && clearlank < 2){//★真芯10
+			setCookie("なんちゃらJUSTMEET",2);
+		}else if(Point.num >= 3000 && clearlank < 1){//★真芯5
+			setCookie("なんちゃらJUSTMEET",1);
+		}
+		*/
 	}
 	//◆バンザイノック
 	else if(course_e === "KNOCK_EX"){
@@ -204,9 +217,15 @@ function cookieUpdate(course_e, Point, ball_number){
 		}else if(Point.num >= 5000 && clearlank < 1){//☆ 5000
 			setCookie("CLEARLANK-KNOCK",1);
 		}
-		//★真芯50
-		//★真芯40
-		//★真芯30
+		/*お好きにどうぞ
+		if(Point.num >= 8000 && clearlank < 3){//★真芯50
+			setCookie("なんちゃらJUSTMEET",3);
+		}else if(Point.num >= 5000 && clearlank < 2){//★真芯40
+			setCookie("なんちゃらJUSTMEET",2);
+		}else if(Point.num >= 3000 && clearlank < 1){//★真芯30
+			setCookie("なんちゃらJUSTMEET",1);
+		}
+		*/
 	}
 	//◆ほのぼの☆☆☆
 	else if(course_e === "EASY_EX"){
@@ -219,8 +238,8 @@ function cookieUpdate(course_e, Point, ball_number){
 			setCookie("CLEARLANK-EASY_EX",1);
 		}
 	}
-		/*
-		◆累計
+		/*お好きにどうぞ
+		◆累計(「TOTAL-なんちゃら」　とかがいいかもね)
 		★初真芯
 		★真芯10
 		★真芯100
@@ -231,10 +250,11 @@ function cookieUpdate(course_e, Point, ball_number){
 
 		★空振り100
 
-		◆特殊
+		◆特殊（「SPECIAL-なんちゃら」　とか？）
 		★合計365m
 		★合計3桁ゾロ目
 		*/
+
 	//アンロック判定
 	unlock();
 }
